e2ed2dbbfd8c7f35139c668162abb5a382404725
PatrickAminGH <patrick.ihab.amin@gmail.com>
2026-05-18 17:31:43 +0300

    Update home-animation.js

--- home-animation.js
+++ home-animation.js
@@ -1,8 +1,8 @@
 const serviceCardsCnt = document.querySelector(".service .cards-cnt");
 const commentsCardsCnt = document.querySelector(".comments .cards-cnt");
 const observer = new IntersectionObserver((entries) => {
-    if(entries[0].isIntersecting) {
-        entries[0].target.classList.add("show");
+    if(entries[0].isIntersecting || entries[1].isIntersecting) {
+        entries[0]?.target.classList.add("show");
         entries[1]?.target.classList.add("show");
     }
 }, {});
